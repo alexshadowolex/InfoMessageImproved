d0bf99d68e78bc19af73d351dbbe5b3505027d96
alexshadowolex <>
2026-03-27 17:23:14 +0100

    Dependency und versions migrated, build fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,16 @@
-import org.jetbrains.compose.compose
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    val kotlinVersion: String by System.getProperties()
+    val kotlinVersion = "2.3.0"
 
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
-    id("org.jetbrains.compose") version "1.2.0-alpha01-dev770"
+
+    id("org.jetbrains.kotlin.plugin.compose") version kotlinVersion
+
+    id("org.jetbrains.compose") version "1.7.0"
 }
 
 group = "me.shika"
@@ -19,13 +22,17 @@ repositories {
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
+kotlin {
+    jvmToolchain(17)
+}
+
 dependencies {
-    val ktorVersion = "2.1.0"
+    val ktorVersion = "3.4.0"
 
     implementation(compose.desktop.currentOs)
 
-    implementation("org.slf4j:slf4j-simple:1.7.36")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
+    implementation("org.slf4j:slf4j-simple:2.0.17")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
     implementation("com.github.tkuenneth:nativeparameterstoreaccess:0.1.2")
 
@@ -37,11 +44,18 @@ tasks.withType<KotlinCompile>() {
         languageSettings.apply {
             optIn("kotlin.RequiresOptIn")
             optIn("kotlin.time.ExperimentalTime")
-            optIn("androidx.compose.ui.ExperimentalComposeUiApi")
         }
     }
 
-    kotlinOptions.jvmTarget = "17"
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+
+        freeCompilerArgs.addAll(
+            "-Xcontext-receivers",
+            "-opt-in=kotlin.RequiresOptIn",
+            "-opt-in=kotlin.time.ExperimentalTime"
+        )
+    }
 }
 
 compose.desktop {
